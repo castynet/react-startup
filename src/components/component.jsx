0c3491d44b45e632043a6df9177c26dc850002fb
castynet <riungemaina@gmail.com>
2020-10-21 11:57:26 +0300

    components/component: created

---
+++ src/components/component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+// import {} from "./utilities"
+
+export default function App() {
+  return (
+    <>
+      <Wrapper>
+        <p>this is the start of something great...</p>
+      </Wrapper>
+    </>
+  );
+}
+
+const Wrapper = styled.div``;
